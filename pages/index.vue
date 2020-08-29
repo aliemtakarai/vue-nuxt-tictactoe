79fb09befec62e93e1573e5f0ceb8ae598b067ca
aliemtakarai <aliemtakarai@gmail.com>
2020-08-29 10:21:39 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div>
+      <TicTacToe />
+    </div>
+  </div>
+</template>
+
+<script>
+import TicTacToe from "~/components/TicTacToe"
+
+export default {
+  components: {
+    TicTacToe
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
